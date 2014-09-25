525616bfe3b995fd431ab2945947e2d1d527a3de
John Smart <john.smart@wakaleo.com>
2014-09-25 22:26:26 +1000

    Added reset feature

--- flights-web-service/src/main/java/flyinghigh/services/flights/web/AirportsController.java
+++ flights-web-service/src/main/java/flyinghigh/services/flights/web/AirportsController.java
@@ -2,9 +2,11 @@ package flyinghigh.services.flights.web;
 
 import flyinghigh.services.flights.domain.Airport;
 import flyinghigh.services.flights.repositories.AirportRepository;
+import flyinghigh.services.flights.services.DatabaseSetup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,11 +14,20 @@ import java.util.List;
 @RestController
 public class AirportsController {
 
-    private @Autowired AirportRepository airportRepository;
+    @Autowired private AirportRepository airportRepository;
+
+    @Autowired
+    private DatabaseSetup databaseSetup;
 
     @RequestMapping("/rest/api/airports")
     public List<Airport> listAirports() {
         return airportRepository.findAll(new Sort("name"));
     }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/rest/api/airports/reset")
+    public void initializeAccounts() {
+        databaseSetup.initializeAirports();;
+    }
+
 
 }
