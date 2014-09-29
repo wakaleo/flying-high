7b17601ac2783bb747d217c1cf179314a931fa00
John Smart <john.smart@wakaleo.com>
2014-09-29 11:50:13 -0700

    Give the accounts page time to load

--- acceptance-tests/src/test/java/flyinghigh/services/acceptancetests/steps/MyAccountUISteps.java
+++ acceptance-tests/src/test/java/flyinghigh/services/acceptancetests/steps/MyAccountUISteps.java
@@ -12,7 +12,7 @@ public class MyAccountUISteps {
     @Step
     public void openAccountPage() {
         myAccountPage.open();
-        myAccountPage.waitFor(250).milliseconds();
+        myAccountPage.waitFor(500).milliseconds();
     }
 
     @Step
